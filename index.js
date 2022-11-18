98102106a64eb2d93fd313730474067f1335c7ae
Tawai-hub <erictash44@gmail.com>
2022-11-18 12:53:52 +0300

    added some js functionality

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-let firstletter = document.querySelector(".first-letter");
+let myBtn = document.querySelector(".btn");
+let section = document.querySelector(".mySection");
+myBtn.addEventListener("click", () => {
+    section.style.backgroundColor = "blue";
+});
